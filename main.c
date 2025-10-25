ed992f4d7de6deaa2f32c62e574dedda089d50a8
Nethushananayakkara <as20240617@sci.sjp.ac.lk>
2025-10-25 13:39:57 +0530

    Initialized constants to be used in the code

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+
+
+#define MAX_CITIES 30
+#define MAX_DELIVERIES 50
+#define MAX_NAME_LEN 50
+#define NUM_VEHICLES 3
+#define FUEL_PRICE 310.0
